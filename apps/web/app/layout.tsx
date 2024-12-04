2452515adf7f935c7a297d7cdc7be5f85b09da22
Michael Kirchner <mkirch@users.noreply.github.com>
2024-12-03 21:15:06 -0600

    lol

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,7 +5,7 @@ import { fonts } from '@repo/design-system/lib/fonts';
 import { Analytics } from '@vercel/analytics/react';
 import type { ReactNode } from 'react';
 import { Footer } from './components/footer';
-import { Header } from './components/header';
+import { Header } from './components/Header';
 
 type RootLayoutProperties = {
   readonly children: ReactNode;
