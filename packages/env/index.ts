692c8b6f15d998b6e2bda536e773e9fd0121b8ac
mkirch <7461887+mkirch@users.noreply.github.com>
2024-12-03 20:19:13 -0600

    Update linter configuration and enhance environment variables
    - Added 'useNamingConvention' rule to 'biome.json' with level set to 'off'.
    - Updated 'turbo.json' to include 'BLOB_READ_WRITE_TOKEN' in the build task environment variables.
    - Refactored environment variable definitions in 'packages/env/index.ts' to ensure consistent usage of 'BLOB_READ_WRITE_TOKEN' across server and client configurations.

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -2,19 +2,17 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 const server: Parameters<typeof createEnv>[0]['server'] = {
-  // Added by Vercel
   VERCEL: z.string().optional(),
   NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
+  BLOB_READ_WRITE_TOKEN: z.string().min(1),
 };
 
 const client: Parameters<typeof createEnv>[0]['client'] = {
-  BLOB_READ_WRITE_TOKEN: z.string().min(1),
   NEXT_PUBLIC_APP_URL: z.string().min(1).url(),
   NEXT_PUBLIC_WEB_URL: z.string().min(1).url(),
   NEXT_PUBLIC_API_URL: z.string().min(1).url().optional(),
   NEXT_PUBLIC_DOCS_URL: z.string().min(1).url().optional(),
   NEXT_PUBLIC_BLOB_STORAGE_DOMAIN: z.string().min(1),
-  // Added by Vercel
   NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().min(1),
 };
 
@@ -24,8 +22,6 @@ export const env = createEnv({
   runtimeEnv: {
     VERCEL: process.env.VERCEL,
     NEXT_RUNTIME: process.env.NEXT_RUNTIME,
-    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
